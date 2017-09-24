a4908df907feab9713f61ba2bf0e5e366228c605
mesuyog <suyog844@gmail.com>
2017-09-24 21:31:01 +1000

    runtime exception if the carpark name is null

--- test/bcccp/tickets/adhoc/TestAdhocTicket.java
+++ test/bcccp/tickets/adhoc/TestAdhocTicket.java
@@ -17,9 +17,22 @@ public class TestAdhocTicket{
         AdhocTicket ticket = new AdhocTicket("0", 113, "A6A09E667F3BCD 2011-12-8,22:13:30.0"); 
         /** Testing CarParkId if the id is null or zero**/
         
-      
+        /** printing the getter TicketNo method**/
+        System.out.println("Unique CarParkId is: "+ ticket.getTicketNo());
+        
+        /** printing the getter CarParkId method**/
+        System.out.println("Unique CarParkId is: "+ ticket.getCarparkId());
+        /** printing the getter BarCode method**/
+        System.out.println("Unique BarCode is: "+ ticket.getBarcode());
+        System.out.println("Unique datetime is: "+ ticket.getEntryDateTime());
+        
+        AdhocTicket test2 = new AdhocTicket("Glenroy001", 113, "A6A09E667F3BCD 2011-12-8,22:13:30.0"); 
+        System.out.println("RuntimeException if the id number is less than or equal to zero "+ test2.getCarparkId());
+        
         AdhocTicket test3 = new AdhocTicket("Glenroy001", 113, "0"); 
         System.out.println("RuntimeException if the barcode number is less than or equal to zero "+ test3.getBarcode());
-          
+        //testing carpark name when it is null
+        AdhocTicket test4 = new AdhocTicket("", 113, "A6A09E667F3BCD 2011-12-8,22:13:30.0"); 
+        System.out.println("RuntimeException if the carpark  is null "+ test4.getCarparkId());
     }
 }
