b8a61a7fb4896113cd4e6f8b2566c0ec9c481927
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-24 23:27:13 +1000

    testing
    unit testing

--- src/bcccp/tickets/season/TestUsageRecord.java
+++ src/bcccp/tickets/season/TestUsageRecord.java
@@ -15,9 +15,12 @@ public class TestUsageRecord {
         /** Creating the instance of Usage Record**/
              //UsageRecord ticket1 = new UsageRecord("S0011", 1203201703 );
              //System.out.println("Unique Season Usage Record is: :  Ticket number "+ ticket1.getSeasonTicketId()+" Season ticket Valid start date and time "+ticket1.getStartTime());
-           System.out.println("Throwing an exception when season ticket id is null ");
-           UsageRecord ticket2 = new UsageRecord("", 120320170);
-           System.out.println("Unique Season Ticket Id is: "+ ticket2.getSeasonTicketId()+" Valid start date "+ticket2.getStartTime());
-           
+           //System.out.println("Throwing an exception when season ticket id is null ");
+           //UsageRecord ticket2 = new UsageRecord("", 120320170);
+           //System.out.println("Unique Season Ticket Id is: "+ ticket2.getSeasonTicketId()+" Valid start date "+ticket2.getStartTime());
+           System.out.println("Throwing an exception when Start Date is less than or equal to zero.");
+           UsageRecord ticket3 = new UsageRecord("S0011",0 ); 
+           System.out.println("Your Ticket Start at : "+ ticket3.getStartTime());
+        
          }
 }
