30657cb7a16d70f080acae41460e7558c4603f78
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-24 23:10:34 +1000

    Testing
    Unit Tesing

--- src/bcccp/tickets/season/TestSeasonTicket.java
+++ src/bcccp/tickets/season/TestSeasonTicket.java
@@ -15,19 +15,16 @@ public class TestSeasonTicket {
         /** printing the main**/        
         System.out.println("This is Testing TestSeasonTicket main before testing....");
         /** Creating the instance of SeasonTicket**/
-       SeasonTicket ticket = new SeasonTicket("S0011","CP3",12022017,120320170 ); 
-        /** printing the getter Ticket Id And Car park  method**/
-        //System.out.println("Unique Season Ticket Id is: "+ ticket.getId());     
-        //System.out.println("Unique CarParkId is: "+ ticket.getCarparkId()); 
-        
-        System.out.println("Throwing an exception when season ticket id is null ");
-        SeasonTicket ticket1 = new SeasonTicket("0","CP3",12022017,120320170);
-        System.out.println("Unique Season Ticket Id is: "+ ticket1.getId());
-        
-        System.out.println("Throwing an exception when carpark name is null ");
-        SeasonTicket ticket2 = new SeasonTicket("S00112","",12022017,120320170);
+       SeasonTicket ticket1 = new SeasonTicket("S0011","CP3",12022017,120320170 );
+       System.out.println("Unique Season Ticket Id is: Season Ticket Id: "+ ticket1.getId()+" Season ticket carpark name:"+ticket1.getCarparkId()+" Valid start date "+ticket1.getStartValidPeriod()+" valid end date: "+ticket1.getEndValidPeriod());
 
-        System.out.println("Unique CarParkId is: "+ ticket2.getCarparkId()); 
+       System.out.println("Throwing an exception when season ticket id is null ");
+       SeasonTicket ticket2 = new SeasonTicket("","CP3",12022017,120320170);
+       System.out.println("Unique Season Ticket Id is: "+ ticket2.getId()+" Season ticket carpark name:"+ticket2.getCarparkId()+" Valid start date "+ticket2.getStartValidPeriod()+" valid end date: "+ticket2.getEndValidPeriod());
+        
+       System.out.println("Throwing an exception when carpark name is null ");
+       SeasonTicket ticket5 = new SeasonTicket("S00112","",12022017,120320170);
+       System.out.println("Unique CarParkId is: "+ ticket5.getCarparkId()); 
         
         System.out.println("Throwing an exception when Start Date is less than or equal to zero.");
         SeasonTicket ticket3 = new SeasonTicket("S0011","CP3",0,120320170 ); 
@@ -37,7 +34,5 @@ public class TestSeasonTicket {
         SeasonTicket ticket4 = new SeasonTicket("S0011","CP3",12032017,120320170 ); 
         System.out.println("Your Ticket Start at : "+ ticket4.getStartValidPeriod());
 
-
-        
     }
 }
--- src/bcccp/tickets/season/TestUsageRecord.java
+++ src/bcccp/tickets/season/TestUsageRecord.java
@@ -10,5 +10,11 @@ package bcccp.tickets.season;
  * @author Rabin Bhattrai
  */
 public class TestUsageRecord {
-    
+         public static void main(String[] args){
+             System.out.println("This is Testing TestUsageRecord main before testing....");
+        /** Creating the instance of Usage Record**/
+             UsageRecord ticket1 = new UsageRecord("S0011", 1203201703 );
+             System.out.println("Unique Season Usage Record is: :  Ticket number "+ ticket1.getSeasonTicketId()+" Season ticket Valid start date and time "+ticket1.getStartTime());
+             
+         }
 }
