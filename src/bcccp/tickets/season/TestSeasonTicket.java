fc7482cad3e2dad16dfefd51b5cdeb2a12c6a690
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-09-24 17:29:34 +1000

    testing

--- src/bcccp/tickets/season/TestSeasonTicket.java
+++ src/bcccp/tickets/season/TestSeasonTicket.java
@@ -5,10 +5,31 @@
  */
 package bcccp.tickets.season;
 
-/**
- *
- * @author Rabin Bhattrai
- */
 public class TestSeasonTicket {
-    
+     
+      public static void main(String[] args){
+         /** printing the main**/        
+         System.out.println("This is Testing TestSeasonTicket main before testing....");
+         /** Creating the instance of SeasonTicket**/
+        SeasonTicket ticket = new SeasonTicket("S0011","CP3",12022017,120320170 ); 
+         /** printing the getter Ticket Id And Car park  method**/
+         //System.out.println("Unique Season Ticket Id is: "+ ticket.getId());     
+         //System.out.println("Unique CarParkId is: "+ ticket.getCarparkId()); 
+         
+         System.out.println("Throwing an exception when season ticket id is null ");
+         SeasonTicket ticket1 = new SeasonTicket("0","CP3",12022017,120320170);
+         System.out.println("Unique Season Ticket Id is: "+ ticket1.getId());
+         
+         System.out.println("Throwing an exception when carpark name is null ");
+          SeasonTicket ticket2 = new SeasonTicket("S00112","",12022017,120320170);
+  
+          System.out.println("Unique CarParkId is: "+ ticket2.getCarparkId()); 
+       
+         System.out.println("Throwing an exception when Start Date is less than or equal to zero.");
+         SeasonTicket ticket3 = new SeasonTicket("S0011","CP3",0,120320170 ); 
+ 
+         System.out.println("Your Ticket Start at : "+ ticket3.getStartValidPeriod()); 
+  
+          
+      }
 }
