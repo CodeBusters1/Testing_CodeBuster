0766ce9ec6cc93849cdc0ce7e59f50cffc2cd34a
basnetroshan <basnet.roshan1991@gmail.com>
2017-09-25 14:40:56 +1000

    EntryController Testing
    Creating object and passing arguments to the constructor

---
+++ src/bcccp/Test/TestEntryController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.Test;
+
+import bcccp.carpark.entry.EntryController;
+/**
+ *
+ * @author roshan
+ */
+public class TestEntryController {
+    public static void main(String[] args){
+        
+EntryController enCon = new EntryController();
+enCon.log("Roshan Basnet");
+    }
+    
+}
